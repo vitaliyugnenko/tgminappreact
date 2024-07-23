2965edfee080fe4e1fda17db2fdb9fbea5632270
Your Name <you@example.com>
2024-07-23 09:05:20 +0300

    rallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const Main = () => {
-  useEffect(() => {
-    // Убедитесь, что Telegram.WebApp доступен
-    if (window.Telegram && window.Telegram.WebApp) {
-      window.Telegram.WebApp.ready();
-      window.Telegram.WebApp.expand();
-
-      // Пример настройки основной кнопки
-      window.Telegram.WebApp.MainButton.text = "Press me";
-      window.Telegram.WebApp.MainButton.show();
-    }
-  }, []);
-
-  return (
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <TonConnectUIProvider manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Main />
   </React.StrictMode>
 );
